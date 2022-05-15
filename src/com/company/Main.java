2dfdeed874160d50da49126bacf16f2706e7ea35
Drew Brokamp <drew.brokamp@gmail.comm>
2022-05-15 15:52:32 -0400

    Allow for user input of a line to test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Linter linter = new Linter();
+        Scanner scanner = new Scanner(System.in);
 
-        String testCodePass = "(var x = {y: [1, 2, 3]})";
-        String testCodeFail = "(var x = {y: [1, 2, 3]";
+        String statementOfJavaScript = scanner.nextLine();
+        linter.linter(statementOfJavaScript);
 
-        linter.linter(testCodePass);
-        linter.linter(testCodeFail);
+//      String testCodePass = "(var x = {y: [1, 2, 3]})";
+//      String testCodeFail = "(var x = {y: [1, 2, 3]";
+
 
     }
 }
